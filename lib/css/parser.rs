07ad2c441f3df7c446661827bfbd608a8eced438
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-05-20 02:11:01 +0900

    :sparkles: Impl CSSOM Parser

---
+++ lib/css/parser.rs
@@ -0,0 +1,472 @@
+use crate::css::structure::{Color, Declaration, Rule, Selector, StyleSheet, Unit, Value};
+use std::iter::Peekable;
+use std::option::Option::Some;
+use std::str::Chars;
+
+#[derive(Debug)]
+pub struct StyleSheetParser<'a> {
+    input: Peekable<Chars<'a>>,
+}
+
+impl<'a> StyleSheetParser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            input: input.chars().peekable(),
+        }
+    }
+
+    pub fn parse(&mut self) -> StyleSheet {
+        let mut rules = vec![];
+        loop {
+            if self.input.peek().is_none() {
+                break;
+            }
+            rules.push(self.parse_rule());
+            self.skip_whitespace();
+        }
+        StyleSheet::new(rules)
+    }
+
+    fn parse_rule(&mut self) -> Rule {
+        let mut selectors = vec![];
+        loop {
+            match self.input.peek().unwrap() {
+                '{' => {
+                    self.bump();
+                    break;
+                }
+                _ => selectors.push(self.parse_selector()),
+            }
+        }
+        let mut declarations = vec![];
+        loop {
+            match self.input.peek().unwrap() {
+                '}' => {
+                    self.bump();
+                    break;
+                }
+                _ => declarations.push(self.parse_declaration()),
+            }
+        }
+        Rule::new(selectors, declarations)
+    }
+
+    fn parse_selector(&mut self) -> Selector {
+        let selector = self.parse_selector_unit();
+        if let Some(',') = self.input.peek() {
+            self.bump()
+        };
+        self.skip_whitespace();
+        selector
+    }
+
+    fn parse_selector_unit(&mut self) -> Selector {
+        self.skip_whitespace();
+        let left = match self.input.peek() {
+            Some('a'..='z' | 'A'..='Z' | '0'..='9') => {
+                let tag_name = self.consume_identifier();
+                Some(Selector::Tag(tag_name))
+            }
+            _ => None,
+        };
+        self.parse_class_selector(left)
+    }
+
+    fn parse_class_selector(&mut self, left: Option<Selector>) -> Selector {
+        match self.input.peek() {
+            Some('.') => {
+                self.input.next();
+                let class = self.consume_identifier();
+                let left = match left {
+                    Some(selector) => Selector::Class(Some(Box::new(selector)), class),
+                    None => Selector::Class(None, class),
+                };
+                self.parse_sibling_selector(Some(left))
+            }
+            Some('#') => {
+                self.input.next();
+                let id = self.consume_identifier();
+                let left = match left {
+                    Some(selector) => Selector::Id(Some(Box::new(selector)), id),
+                    None => Selector::Id(None, id),
+                };
+                self.parse_sibling_selector(Some(left))
+            }
+            Some('+' | '>') => self.parse_sibling_selector(left),
+            _ => left.unwrap(),
+        }
+    }
+
+    // parse_class_selector内に入れることができるが、可読性のため別けた
+    fn parse_sibling_selector(&mut self, left: Option<Selector>) -> Selector {
+        match self.input.peek() {
+            Some('>') => {
+                self.input.next();
+                let right = self.parse_selector_unit();
+                match left {
+                    Some(selector) => {
+                        let left = Selector::Child(Box::new(selector), Box::new(right));
+                        self.parse_sibling_selector(Some(left))
+                    }
+                    None => panic!("Cannot parse right selector"),
+                }
+            }
+            Some('+') => {
+                self.input.next();
+                let right = self.parse_selector_unit();
+                match left {
+                    Some(selector) => {
+                        let left = Selector::Adjacent(Box::new(selector), Box::new(right));
+                        self.parse_sibling_selector(Some(left))
+                    }
+                    None => panic!("Cannot parse right selector"),
+                }
+            }
+            _ => left.unwrap(),
+        }
+    }
+
+    fn parse_declaration(&mut self) -> Declaration {
+        let property = self.consume_identifier();
+        self.skip_next_ch(&':');
+        self.skip_whitespace();
+        let value = match self.input.peek() {
+            Some('#') => {
+                self.input.next();
+                let r = self.consume_hex(2);
+                let g = self.consume_hex(2);
+                let b = self.consume_hex(2);
+                let a = match self.input.peek() {
+                    Some(ch) if matches!(ch, '0'..='9' | 'a'..='z') => self.consume_hex(2),
+                    _ => 0,
+                };
+                Value::Color(Color::new(r, g, b, a))
+            }
+            Some('0'..='9') => {
+                let length = self.consume_number();
+                let unit_ident = self.consume_identifier();
+                let unit = match unit_ident.as_str() {
+                    "px" => Unit::Px,
+                    _ => Unit::Px,
+                };
+                Value::Length(length, unit)
+            }
+            Some('a'..='z' | 'A'..='Z') => {
+                let ident = self.consume_identifier();
+                Value::Other(ident)
+            }
+            _ => panic!("Cannot parse declaration"),
+        };
+        self.skip_next_ch(&';');
+        self.skip_whitespace();
+        Declaration::new(property, value)
+    }
+
+    fn consume_identifier(&mut self) -> String {
+        self.consume(&|ch| matches!(ch, '0'..='9' | 'a'..='z' | 'A'..='Z' | '_'))
+    }
+
+    fn consume_number(&mut self) -> f32 {
+        self.consume(&|ch| matches!(ch, '0'..='9' | '.'))
+            .parse()
+            .unwrap()
+    }
+
+    fn consume_hex(&mut self, n: usize) -> usize {
+        usize::from_str_radix(
+            &self.consume_for(&|ch| matches!(ch, '0'..='9' | 'a'..='f'), n),
+            16,
+        )
+        .unwrap_or_default()
+    }
+
+    fn consume_for<F>(&mut self, consume_condition: &F, nth: usize) -> String
+    where
+        F: Fn(&char) -> bool,
+    {
+        self.skip_whitespace();
+        let mut s = String::new();
+        for _ in 0..nth {
+            match self.input.next_if(consume_condition) {
+                Some(ch) => s.push_str(&ch.to_string()),
+                _ => break,
+            }
+        }
+        self.skip_whitespace();
+        s
+    }
+
+    fn consume<F>(&mut self, consume_condition: &F) -> String
+    where
+        F: Fn(&char) -> bool,
+    {
+        self.skip_whitespace();
+        let mut s = String::new();
+        while let Some(ch) = self.input.next_if(consume_condition) {
+            s.push_str(&ch.to_string());
+        }
+        self.skip_whitespace();
+        s
+    }
+
+    fn skip_next_ch(&mut self, ch: &char) {
+        match self.input.next() {
+            Some(ref c) if c == ch => {}
+            _ => panic!("Cannot found {}", ch),
+        };
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.input.next_if(|&x| x.is_whitespace()).is_some() {}
+    }
+
+    fn bump(&mut self) {
+        match self.input.next() {
+            Some(c) => c,
+            None => panic!("Cannot bump"),
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::css::parser::StyleSheetParser;
+    use crate::css::structure::{Color, Declaration, Rule, Selector, StyleSheet, Unit, Value};
+
+    fn new(input: &str) -> StyleSheetParser {
+        StyleSheetParser::new(input)
+    }
+
+    #[test]
+    fn test_parse() {
+        let mut parser = StyleSheetParser::new(
+            r#"
+div > .table {
+    margin: auto ; 
+    padding : 10.5 px; 
+    color: #aa11ff22; 
+}
+
+
+#answer, h1 {
+    display: none;
+}
+"#,
+        );
+        let selectors = vec![Selector::Child(
+            Box::new(Selector::Tag("div".to_string())),
+            Box::new(Selector::Class(None, "table".to_string())),
+        )];
+        let declarations = vec![
+            Declaration::new("margin".to_string(), Value::Other("auto".to_string())),
+            Declaration::new("padding".to_string(), Value::Length(10.5, Unit::Px)),
+            Declaration::new(
+                "color".to_string(),
+                Value::Color(Color::new(0xaa, 0x11, 0xff, 0x22)),
+            ),
+        ];
+        let rule1 = Rule::new(selectors, declarations);
+
+        let selectors = vec![
+            Selector::Id(None, "answer".to_string()),
+            Selector::Tag("h1".to_string()),
+        ];
+        let declarations = vec![Declaration::new(
+            "display".to_string(),
+            Value::Other("none".to_string()),
+        )];
+        let rule2 = Rule::new(selectors, declarations);
+
+        let expect = StyleSheet::new(vec![rule1, rule2]);
+        assert_eq!(parser.parse(), expect);
+    }
+
+    #[test]
+    fn test_parse_rule() {
+        let mut parser = StyleSheetParser::new(
+            r#"
+div > .table {
+    margin: auto ; 
+    padding : 10.5 px; 
+    color: #aa11ff22; 
+}"#,
+        );
+        let selectors = vec![Selector::Child(
+            Box::new(Selector::Tag("div".to_string())),
+            Box::new(Selector::Class(None, "table".to_string())),
+        )];
+        let declarations = vec![
+            Declaration::new("margin".to_string(), Value::Other("auto".to_string())),
+            Declaration::new("padding".to_string(), Value::Length(10.5, Unit::Px)),
+            Declaration::new(
+                "color".to_string(),
+                Value::Color(Color::new(0xaa, 0x11, 0xff, 0x22)),
+            ),
+        ];
+        let expect = Rule::new(selectors, declarations);
+        assert_eq!(parser.parse_rule(), expect);
+    }
+
+    #[test]
+    fn test_parse_selector_unit() {
+        let tests = vec![
+            (new("div"), Selector::Tag("div".to_string())),
+            (new(".box"), Selector::Class(None, "box".to_string())),
+            (
+                new("p.box"),
+                Selector::Class(
+                    Some(Box::new(Selector::Tag("p".to_string()))),
+                    "box".to_string(),
+                ),
+            ),
+            (new("#box"), Selector::Id(None, "box".to_string())),
+            (
+                new("p#box"),
+                Selector::Id(
+                    Some(Box::new(Selector::Tag("p".to_string()))),
+                    "box".to_string(),
+                ),
+            ),
+            (
+                new("head > div"),
+                Selector::Child(
+                    Box::new(Selector::Tag("head".to_string())),
+                    Box::new(Selector::Tag("div".to_string())),
+                ),
+            ),
+            (
+                new("head > div > p"),
+                Selector::Child(
+                    Box::new(Selector::Tag("head".to_string())),
+                    Box::new(Selector::Child(
+                        Box::new(Selector::Tag("div".to_string())),
+                        Box::new(Selector::Tag("p".to_string())),
+                    )),
+                ),
+            ),
+            (
+                new("head + div"),
+                Selector::Adjacent(
+                    Box::new(Selector::Tag("head".to_string())),
+                    Box::new(Selector::Tag("div".to_string())),
+                ),
+            ),
+            (
+                new("head + div + p"),
+                Selector::Adjacent(
+                    Box::new(Selector::Tag("head".to_string())),
+                    Box::new(Selector::Adjacent(
+                        Box::new(Selector::Tag("div".to_string())),
+                        Box::new(Selector::Tag("p".to_string())),
+                    )),
+                ),
+            ),
+            (
+                new(".table > p"),
+                Selector::Child(
+                    Box::new(Selector::Class(None, "table".to_string())),
+                    Box::new(Selector::Tag("p".to_string())),
+                ),
+            ),
+            (
+                new(".table > #box"),
+                Selector::Child(
+                    Box::new(Selector::Class(None, "table".to_string())),
+                    Box::new(Selector::Id(None, "box".to_string())),
+                ),
+            ),
+            (
+                new("div.table > p#box"),
+                Selector::Child(
+                    Box::new(Selector::Class(
+                        Some(Box::new(Selector::Tag("div".to_string()))),
+                        "table".to_string(),
+                    )),
+                    Box::new(Selector::Id(
+                        Some(Box::new(Selector::Tag("p".to_string()))),
+                        "box".to_string(),
+                    )),
+                ),
+            ),
+        ];
+        for (mut actual, expect) in tests {
+            assert_eq!(actual.parse_selector_unit(), expect);
+        }
+    }
+
+    #[test]
+    fn test_parse_declaration() {
+        let mut parser = StyleSheetParser::new("margin: auto ;");
+        assert_eq!(
+            parser.parse_declaration(),
+            Declaration::new("margin".to_string(), Value::Other("auto".to_string()))
+        );
+        let mut parser = StyleSheetParser::new("padding : 10.5 px;");
+        assert_eq!(
+            parser.parse_declaration(),
+            Declaration::new("padding".to_string(), Value::Length(10.5, Unit::Px))
+        );
+        let mut parser = StyleSheetParser::new("color: #aa11ff22;");
+        assert_eq!(
+            parser.parse_declaration(),
+            Declaration::new(
+                "color".to_string(),
+                Value::Color(Color::new(0xaa, 0x11, 0xff, 0x22))
+            )
+        );
+        let mut parser = StyleSheetParser::new("color: #aa11ff ;");
+        assert_eq!(
+            parser.parse_declaration(),
+            Declaration::new(
+                "color".to_string(),
+                Value::Color(Color::new(0xaa, 0x11, 0xff, 0x00))
+            )
+        );
+    }
+
+    #[test]
+    fn test_consume_identifier() {
+        let mut parser = StyleSheetParser::new("h_1, h_2");
+        assert_eq!(parser.consume_identifier(), "h_1".to_string());
+    }
+
+    #[test]
+    fn test_consume_hex() {
+        let mut parser = StyleSheetParser::new("ffaa");
+        assert_eq!(parser.consume_hex(2), 0xff);
+    }
+
+    #[test]
+    fn test_consume_number() {
+        let input = "10";
+        let mut parser = StyleSheetParser::new(input);
+        assert_eq!(parser.consume_number(), 10 as f32);
+    }
+
+    #[test]
+    fn test_consume_for() {
+        let mut parser = StyleSheetParser::new("h1h2");
+        assert_eq!(
+            parser.consume_for(&|ch| ch.is_ascii_alphanumeric(), 2),
+            "h1".to_string(),
+        );
+    }
+
+    #[test]
+    fn test_consume() {
+        let input = "h1, h2";
+        let mut parser = StyleSheetParser::new(input);
+        assert_eq!(
+            parser.consume(&|ch| ch.is_ascii_alphanumeric()),
+            "h1".to_string(),
+        );
+    }
+
+    #[test]
+    fn test_skip_whitespace() {
+        let input = "   x ";
+        let mut parser = StyleSheetParser::new(input);
+        parser.skip_whitespace();
+        assert_eq!(parser.input.next().unwrap(), 'x');
+    }
+}
--- lib/css/structure.rs
+++ lib/css/structure.rs
@@ -1,13 +1,13 @@
 use std::fmt;
 use std::fmt::Debug;
 
-#[derive(Default)]
+#[derive(Default, PartialEq)]
 pub struct StyleSheet {
     pub rules: Vec<Rule>,
 }
 
-// h1, h2, div.note, #answer { margin: auto, color: #cc0000 }
-#[derive(Default)]
+// h1, h2, div.note, #answer { margin: auto; color: #cc0000 }
+#[derive(Default, PartialEq)]
 pub struct Rule {
     pub selectors: Vec<Selector>,       // h1, h2, h3, div.note, #answer
     pub declarations: Vec<Declaration>, // { margin: auto; color: #cc0000; }
@@ -15,38 +15,35 @@ pub struct Rule {
 
 // div > p
 // div + p
-#[derive(Default)]
-pub struct Selector {
-    pub simple: Vec<SimpleSelector>, // div, p
-    pub combinators: Vec<char>,      // >, +, etc.
-}
-
 // h1
 // answer
 // .note
-#[derive(Default)]
-pub struct SimpleSelector {
-    pub tag_name: Option<String>, // h1, h2, h3, div
-    pub id: Option<String>,       // answer
-    pub classes: Vec<String>,     // .note
+#[derive(PartialEq)]
+pub enum Selector {
+    Tag(String),                            // h1, div, etc.
+    Class(Option<Box<Selector>>, String),   // .note, div.note, etc.
+    Id(Option<Box<Selector>>, String),      // #note, div#note, etc.
+    Child(Box<Selector>, Box<Selector>),    // article > p
+    Adjacent(Box<Selector>, Box<Selector>), // h1 + p
 }
 
 // margin: 10px
 // div: #cc0000
 // display: none
-#[derive(Debug, Default)]
+#[derive(Default, PartialEq)]
 pub struct Declaration {
     pub property: String, // margin, padding, display, etc.
     pub value: Value,     // #cc0000, 10px, etc.
 }
 
+#[derive(PartialEq)]
 pub enum Value {
     Color(Color),      // #cc0000
     Length(f32, Unit), // 20px
     Other(String),     // auto, none
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Unit {
     Em,
     Ex,
@@ -66,12 +63,12 @@ pub enum Unit {
     Pct,
 }
 
-#[derive(Default)]
+#[derive(Default, PartialEq)]
 pub struct Color {
-    pub r: f32,
-    pub g: f32,
-    pub b: f32,
-    pub a: f32,
+    pub r: usize,
+    pub g: usize,
+    pub b: usize,
+    pub a: usize,
 }
 
 impl StyleSheet {
@@ -84,7 +81,7 @@ impl fmt::Debug for StyleSheet {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut rules = String::new();
         for rule in &self.rules {
-            if rules.len() > 0 {
+            if !rules.is_empty() {
                 rules.push_str("\n\n");
             }
             rules.push_str(&format!("{:?}", rule));
@@ -118,70 +115,40 @@ impl fmt::Debug for Rule {
         for declaration in &self.declarations {
             declarations.push_str(tab);
             declarations.push_str(&format!("{:?}", declaration));
-            declarations.push_str("\n");
+            declarations.push('\n');
         }
 
         write!(f, "{} {{\n{}}}", selectors, declarations)
     }
 }
 
-impl Selector {
-    pub fn new(simple: Vec<SimpleSelector>, combinators: Vec<char>) -> Self {
-        Self {
-            simple,
-            combinators,
-        }
-    }
-}
-
 impl fmt::Debug for Selector {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut simple = String::new();
-        for ss in &self.simple {
-            if !simple.is_empty() {
-                simple.push_str(", ");
-            }
-            simple.push_str(&format!("{:?}", ss));
+        match self {
+            Selector::Tag(ref tag) => write!(f, "{}", tag),
+            Selector::Class(s, class) => match s {
+                Some(selector) => write!(f, "{:?}.{}", selector, class),
+                None => write!(f, ".{}", class),
+            },
+            Selector::Id(s, id) => match s {
+                Some(selector) => write!(f, "{:?}#{}", selector, id),
+                None => write!(f, "#{}", id),
+            },
+            Selector::Child(p, c) => write!(f, "{:?} > {:?}", p, c),
+            Selector::Adjacent(l, r) => write!(f, "{:?} + {:?}", l, r),
         }
-        write!(f, "{}", simple)
-    }
-}
-
-impl SimpleSelector {
-    pub fn new(tag_name: Option<String>, id: Option<String>, classes: Vec<String>) -> Self {
-        Self {
-            tag_name,
-            id,
-            classes,
-        }
-    }
-}
-
-impl fmt::Debug for SimpleSelector {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut result = String::new();
-
-        if let Some(ref t) = self.tag_name {
-            result.push_str(t)
-        }
-
-        if let Some(ref s) = self.id {
-            result.push('#');
-            result.push_str(s);
-        }
-
-        for class in &self.classes {
-            result.push('.');
-            result.push_str(class);
-        }
-
-        write!(f, "{}", result)
     }
 }
 
 impl Declaration {
     pub fn new(property: String, value: Value) -> Self {
         Self { property, value }
+    }
+}
+
+impl fmt::Debug for Declaration {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}: {:?}", self.property, self.value)
     }
 }
 
@@ -202,7 +169,7 @@ impl fmt::Debug for Value {
 }
 
 impl Color {
-    pub fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
+    pub fn new(r: usize, g: usize, b: usize, a: usize) -> Self {
         Self { r, g, b, a }
     }
 }
@@ -210,5 +177,50 @@ impl Color {
 impl fmt::Debug for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "r: {} g: {} b: {} a: {}", self.r, self.g, self.b, self.a)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::css::structure::Selector;
+
+    #[test]
+    fn test_debug_selector() {
+        let tests = vec![
+            (Selector::Tag("div".to_string()), "div".to_string()),
+            (Selector::Class(None, "box".to_string()), ".box".to_string()),
+            (
+                Selector::Class(
+                    Some(Box::new(Selector::Tag("div".to_string()))),
+                    "box".to_string(),
+                ),
+                "div.box".to_string(),
+            ),
+            (Selector::Id(None, "box".to_string()), "#box".to_string()),
+            (
+                Selector::Id(
+                    Some(Box::new(Selector::Tag("div".to_string()))),
+                    "box".to_string(),
+                ),
+                "div#box".to_string(),
+            ),
+            (
+                Selector::Child(
+                    Box::new(Selector::Tag("article".to_string())),
+                    Box::new(Selector::Tag("p".to_string())),
+                ),
+                "article > p".to_string(),
+            ),
+            (
+                Selector::Adjacent(
+                    Box::new(Selector::Tag("h1".to_string())),
+                    Box::new(Selector::Tag("p".to_string())),
+                ),
+                "h1 + p".to_string(),
+            ),
+        ];
+        for (actual, expect) in tests {
+            assert_eq!(format!("{:?}", actual), expect)
+        }
     }
 }
